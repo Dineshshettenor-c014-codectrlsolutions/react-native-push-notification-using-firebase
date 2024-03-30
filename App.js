57093d15d90ac1969f382610298f85308b3dae42
Dinesh Shettenor - C014 - Code Ctrl Solutions Private Limited <129173689+Dineshshettenor-c014-codectrlsolutions@users.noreply.github.com>
2024-03-30 13:20:32 +0530

    added the simple push notification using firebase

--- App.js
+++ App.js
@@ -1,7 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 
 export default function App() {
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  useEffect(() => {
+    if (requestUserPermission()) {
+      // return fcm token for the device
+      messaging().getToken().then(token => {
+        console.log(token);
+      })
+    } else {
+      console.log("Failed token status", authStatus);
+    }
+
+    //getInitialNotification: When the application is opened from a quit state.
+    messaging()
+      .getInitialNotification()
+      .then(async (remoteMessage) => {
+        if (remoteMessage) {
+          console.log("Notification caused app to open from quit state : ", remoteMessage.notification,);
+        }
+      })
+
+    // Assume a message-notification contains a "type" property in the data payload of the screen to open
+
+    messaging().onNotificationOpenedApp(async (remoteMessage) => {
+      console.log("Notification causes app to open from background state:", remoteMessage.notification,);
+    })
+
+    // Register background handler
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
+
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, [])
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
